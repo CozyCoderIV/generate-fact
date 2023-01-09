5edfce959355bf14468dd545bf1ca3e8feebad7f
Otis Jackson IV <spiritedhokage@gmail.com>
2023-01-09 12:37:06 -0600

    First API project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
+import { useState, useEffect } from 'react';
+
+// // function used to fetch data from an API   (inefficient)
+// fetch("https://catfact.ninja/fact")
+//  .then((resp) => resp.json())
+//  .then((data) => {
+//   console.log(data);
+// }); 
+
+// javascript library -> axios makes getting data from API's easier
 
 function App() {
+  const [catFact, setCatFact] = useState("");
+  const generateFact = () => {
+    let newFact = ""
+    Axios.get("https://catfact.ninja/fact").then((resp) => {
+      newFact = resp.data.fact;
+      setCatFact(newFact);
+    })
+  }
+
+  useEffect(()=>{
+    generateFact();
+  }, []);
+  
+  // // (inefficient) -> updates infinitely
+
+  // Axios.get("https://catfact.ninja/fact").then((resp) => {
+  //   // console.log(resp.data);
+  //   setCatFact(resp.data.fact);
+  // })
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button id="gen_button" onClick={generateFact}>Generate Cat Fact</button>
+      <p>{catFact}</p>
     </div>
   );
 }
